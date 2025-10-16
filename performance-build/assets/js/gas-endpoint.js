14bd91f678a288297c22570cc0ea520cd9293c12
Nathan Johnson <warriorgen.2025@gmail.com>
2025-10-16 12:12:30 +0200

    REFINE: Update GAS endpoint URL and versioning for improved integration

--- performance-build/assets/js/gas-endpoint.js
+++ performance-build/assets/js/gas-endpoint.js
@@ -2,14 +2,14 @@
 // Load this BEFORE config.js on any page that talks to Apps Script.
 (function () {
   // UPDATE THESE TWO VALUES ONLY when you redeploy Apps Script:
-  const CALENDAR_URL = 'https://script.google.com/macros/s/AKfycbwpNDjEejgORzPQ9AzCl3xkqOB5Ane5wwvRvnN5Gj2gxsdU9RRUYdwNsY8dDPm6sjaUwg/exec';
+  const CALENDAR_URL = 'https://script.google.com/macros/s/AKfycbwTlKLd_ijdqnZPZx4G6Jo_lGGZipMNlwaga6mz5kz395w3E9b_MqYbX0aU1mDJXbVz6Q/exec';
   const SHARED_KEY   = '8bd4cad053faee35a6e87af44d78622122fd8bce954c91c20e7412f56947558c1d0d2de4e2f6f83e883194db9012b2c1';
 
   // Publish globally so config.js (or any script) can read it.
   window.SF_GAS = {
     CALENDAR_URL,
     SHARED_KEY,
-    version: '2025-10-16-5' // bump when you change the URL/key
+  version: '2025-10-16-6' // bump when you change the URL/key
   };
 
   // Helpful console breadcrumb
